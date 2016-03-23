1d1c5fbd6382057b6becadeadb4209ed778f954e
Jeremiah Wala <jwala@broadinstitute.org>
2016-03-23 13:57:44 -0400

    build fix

--- install_bioc.sh
+++ install_bioc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-Rscript -e 'source("https://bioconductor.org/biocLite.R"); biocLite("BiocInstaller"); biocLite("BSgenome.Hsapiens.UCSC.hg19"); biocLite("GenomicRanges"); devtools::install_github("devtools/gUtils"); biocLite("Rcytoscape"); biocList("VariantAnnotation"); biocList("graph")'
+Rscript -e 'install.packages("devtools"); source("https://bioconductor.org/biocLite.R"); biocLite("BiocInstaller"); biocLite("BSgenome.Hsapiens.UCSC.hg19"); biocLite("GenomicRanges"); devtools::install_github("devtools/gUtils"); biocLite("Rcytoscape"); biocList("VariantAnnotation"); biocList("graph")'
